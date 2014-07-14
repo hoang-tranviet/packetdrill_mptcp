48339e9bdb15ea2921d40e1c401361a95f8464de
Christoph Paasch <christoph.paasch@uclouvain.be>
2014-07-14 23:01:09 +0200

    cork
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

---
+++ gtests/net/packetdrill/examples/tests/tcp_headers/run_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+for f in `find . -name "*.pkt" | sort`; do
+  echo "Running $f ..."
+  ip tcp_metrics flush all > /dev/null 2>&1
+  packetdrill --tolerance_usecs=30000 $f
+done
+
