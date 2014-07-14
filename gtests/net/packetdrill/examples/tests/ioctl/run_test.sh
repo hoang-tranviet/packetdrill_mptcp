16f1e9e349dd95d5ae3ffd24768aa22f72961927
Christoph Paasch <christoph.paasch@uclouvain.be>
2014-07-14 22:28:44 +0200

    more tests
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- gtests/net/packetdrill/examples/tests/ioctl/run_test.sh
+++ gtests/net/packetdrill/examples/tests/ioctl/run_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 for f in `find . -name "*.pkt" | sort`; do
   echo "Running $f ..."
   ip tcp_metrics flush all > /dev/null 2>&1
-  packetdrill $f
+  packetdrill --tolerance_usecs=20000 $f
 done
 
