52557c1516ee08410821c4c4c6ae4f009961d7d0
Christoph Paasch <christoph.paasch@uclouvain.be>
2014-07-15 17:23:56 +0200

    tests
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- gtests/net/packetdrill/examples/mptcp/run_test.sh
+++ gtests/net/packetdrill/examples/mptcp/run_test.sh
@@ -4,6 +4,6 @@ set -e
 for f in `find . -name "*.pkt" | sort`; do
   echo "Running $f ..."
   ip tcp_metrics flush all > /dev/null 2>&1
-  packetdrill --tolerance_usecs=20000 $f
+  packetdrill --tolerance_usecs=40000 $f
 done
 
