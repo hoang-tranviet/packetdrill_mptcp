b0f3c79631d0497d5383c8199917bc8b221ce649
Christoph Paasch <christoph.paasch@uclouvain.be>
2014-07-16 11:29:46 +0200

    ICMP
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- gtests/net/packetdrill/examples/tests/run_test.sh
+++ gtests/net/packetdrill/examples/tests/run_test.sh
@@ -3,6 +3,6 @@ set -e
 for f in `find . -name "*.pkt" | sort`; do
   echo "Running $f ..."
   ip tcp_metrics flush all > /dev/null 2>&1
-  packetdrill $f
+  packetdrill --tolerance_usecs=30000 $f
 done
 
