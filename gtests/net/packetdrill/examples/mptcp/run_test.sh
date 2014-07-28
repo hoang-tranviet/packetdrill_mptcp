4757b91771d8918582e20a109c24b9cb1ce95947
Christoph Paasch <christoph.paasch@uclouvain.be>
2014-07-29 00:55:06 +0200

    More tests
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- gtests/net/packetdrill/examples/mptcp/run_test.sh
+++ gtests/net/packetdrill/examples/mptcp/run_test.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 set -e
 
+source ~/high_tolerance
+
+function find_string() {
+	for x in `seq 2 $#`; do
+		if [[ $1 == *${!x}* ]]
+		then
+			return 0
+		fi
+	done
+
+	return 1
+}
+
 for f in `find . -name "*.pkt" | sort`; do
   echo "Running $f ..."
   ip tcp_metrics flush all > /dev/null 2>&1
-  packetdrill --tolerance_usecs=40000 $f
+  TOLERANCE=40000
+  if find_string $f $HIGH_TOLERANCE ; then
+	TOLERANCE=220000
+	echo "High tolerance test"
+  fi
+  packetdrill --tolerance_usecs=$TOLERANCE $f
 done
 
